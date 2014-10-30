a769ce3af1f2feac87ccbd0f8de5f72b83cc58ab
daviigor <daviscrazy87@gmail.com>
2014-10-30 18:06:56 -0200

    commit teste

---
+++ OBJTRANSFERENCIA/Endereco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJTRANSFERENCIA
+{
+    public class Endereco
+    {
+        public string strLogradouro { get; set; }
+        public string strNumero { get; set; }
+        public string strBairro { get; set; }
+        public Int32 intCodigoIbgeMunicipio { get; set; }
+        public string strMunicipio { get; set; }
+        public string strUF { get; set; }
+        public string strCep { get; set; }
+        public Int32 intCodigoPais { get; set; }
+        public string strPais { get; set; }
+        public string strTelefone { get; set; }
+    }
+    public class EnderecoColecao : List<Endereco> { }
+}
