b805add74438f3c0b80457fc1962b49c2e8aed40
daviigor <daviscrazy87@gmail.com>
2014-10-30 18:06:13 -0200

    commit test

---
+++ OBJTRANSFERENCIA/NFEEmitente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJTRANSFERENCIA
+{
+    public class NfeEmitente
+    {
+        public string strCnpj { get; set; }
+        public string strNome { get; set; }
+        public string strNomeFantasia { get; set; }
+        public Endereco endEndereco { get; set; }
+        public string strInscricaoEstadual { get; set; }
+        public Int32 intCRT { get; set; }
+    }
+    public class NFEEmitenteColecao : List<NfeEmitente> { }
+}
