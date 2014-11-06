251458f06968ae82f66364fdc815f2de31bee3e6
daviigor <daviscrazy87@gmail.com>
2014-11-06 15:04:04 -0200

    Novos objetos de transferencia criados

---
+++ OBJTRANSFERENCIA/Transportador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJTRANSFERENCIA
+{
+    public class Transportador
+    {
+        public string strCnpj { get; set; }
+        public string strNome { get; set; }
+        public string strIncricaoEstadual { get; set; }
+        public string strEndereco { get; set; }
+    }
+}
---
+++ OBJTRANSFERENCIA/Veiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBJTRANSFERENCIA
+{
+    public class Veiculo
+    {
+        public string strRenavam { get; set; }
+        public string strPlaca { get; set; }
+        public string strUf { get; set; }
+        public string strRntc { get; set; }
+
+    }
+
+    public class VeiculoColecao : List<Veiculo> { }
+}
